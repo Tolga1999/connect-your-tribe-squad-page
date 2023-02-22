834444b0bf4a5518b2c21b0e24102388b634c65d
Tolga1999 <tolgadoganer@outlook.com>
2023-02-22 14:32:37 +0100

    een meer advanced zoekfunctie

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,38 +1,21 @@
 // THIS IS A CLIENT SIDE SOLUTION
-
-// select elements
-let input = document.querySelector("input");
+// select form + select all student containers
 let form = document.querySelector("form");
-
-// select all h2 names
-let studentNames = document.querySelectorAll(".student-name")
-// select all student containers
 let studentContainer = document.querySelectorAll(".student-container");
-// select all names
-let allNames = document.querySelectorAll(".only-name");
-// console.log(allNames[0].innerHTML.toUpperCase())
-// console.log(studentContainer[0].children[1].children[1].innerHTML)
 
-// loop the names for use
-form.addEventListener('submit', function (e) {
+// loop the names
+form.addEventListener('input', function (e) {
     e.preventDefault();
-    for (let i = 0; i < studentContainer.length; i++) {
-        // toUppercase to make the comparison easier
-        // if (input.value.toUpperCase() !== studentContainer[i].children[1].children[1].innerHTML.toUpperCase()) {
-        //     studentContainer[i].classList.add("container-off");
-        // } else {
-        //     studentContainer[i].classList.remove("container-off")
-        // }
 
+    // selecteer input
+    let input = document.querySelector("input").value.toUpperCase();
+
+    for (let i = 0; i < studentContainer.length; i++) {
+        
         // search on first name
-        if (input.value.toUpperCase() !== allNames[i].innerHTML.toUpperCase()) {
+        if (!studentContainer[i].children[1].children[1].innerHTML.toUpperCase().includes(input)) {
             studentContainer[i].classList.add("container-off");
         } else {
-            studentContainer[i].classList.remove("container-off")
-        }  
-        
-         // if field is empty show all
-         if (input.value === "") {
             studentContainer[i].classList.remove("container-off")
         }
     }
