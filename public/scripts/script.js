3f01dbc76104e954701abf061594f3fa53608cf7
Tolga1999 <tolgadoganer@outlook.com>
2023-02-22 04:09:24 +0100

    kleine aanpassing

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -18,23 +18,23 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
     for (let i = 0; i < studentContainer.length; i++) {
         // toUppercase to make the comparison easier
-        if (input.value.toUpperCase() !== studentContainer[i].children[1].children[1].innerHTML.toUpperCase()) {
-            studentContainer[i].classList.add("container-off");
-        } else {
-            studentContainer[i].classList.remove("container-off")
-        }
-
-        // if field is empty show all
-        if (input.value === "") {
-            studentContainer[i].classList.remove("container-off")
-        }
-
-        // search on first name
-        // if (input.value.toUpperCase() !== allNames[i].innerHTML.toUpperCase()) {
+        // if (input.value.toUpperCase() !== studentContainer[i].children[1].children[1].innerHTML.toUpperCase()) {
         //     studentContainer[i].classList.add("container-off");
         // } else {
         //     studentContainer[i].classList.remove("container-off")
-        // }   
+        // }
+
+        // search on first name
+        if (input.value.toUpperCase() !== allNames[i].innerHTML.toUpperCase()) {
+            studentContainer[i].classList.add("container-off");
+        } else {
+            studentContainer[i].classList.remove("container-off")
+        }  
+        
+         // if field is empty show all
+         if (input.value === "") {
+            studentContainer[i].classList.remove("container-off")
+        }
     }
 
 })
